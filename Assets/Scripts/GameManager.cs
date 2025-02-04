b633e7b9421a3ed5be20685dd9b76e4e21057849
eJwtjrtOAzEQAP9laxf3CDrHHRISpxRAEaC01vY6MefY1vqOEKL8OyK6dqQZzRXQWqp1nydKoMB40/hG2t4MrZQerez6bUNucBty/XbYGGrxQToQUDh/B0e8qte1pOc1dThmvR9fa9rhe+nHdnx5/n2cuxE/dse3T2/rU5e+0jKBgLui50uh/wVCJgYB1eY7QHcKSZXFxGD1RBfBhE5lPgimksVSiRWdMERxzjz5mM8ggH5KYKoaZ1BpifF2+wP+UFEZ <xenia.aguilar.7e7@itb.cat>
2025-02-04 15:13:56 +0100

    dddd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public int Orbs = 0, Coins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if(GameManager.gameManager != null && GameManager.gameManager != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            GameManager.gameManager =  this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void OrbCollected()
+    {
+        Orbs++;
+    }
+     public void CoinCollected()
+    {
+        Coins++;
+    }
+
+    public interface ICollectable
+    {
+        public void OnCollected();
+    }
+}
+
