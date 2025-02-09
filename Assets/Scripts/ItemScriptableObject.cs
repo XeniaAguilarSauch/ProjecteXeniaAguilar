c6de4c6adbf9b11b56749f5e86bece15249befba
eJwtjrtOAzEQAP9laxf3CDrHHRISpxRAEaC01vY6MefY1vqOEKL8OyK6dqQZzRXQWqp1nydKoMB40/hG2t4MrZQerez6bUNucBty/XbYGGrxQToQUDh/B0e8qte1pOc1dThmvR9fa9rhe+nHdnx5/n2cuxE/dse3T2/rU5e+0jKBgLui50uh/wVCJgYB1eY7QHcKSZXFxGD1RBfBhE5lPgimksVSiRWdMERxzjz5mM8ggH5KYKoaZ1BpifF2+wP+UFEZ <xenia.aguilar.7e7@itb.cat>
2025-02-09 23:42:09 +0100

    ffff
    ggg

---
+++ Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Items")]
+
+public class ItemScriptableObject : ScriptableObject
+{
+    public Sprite image;
+    public int price;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
