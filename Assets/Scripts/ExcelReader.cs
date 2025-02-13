9382c3282a54d5c6669e7a3ae8e7c3f908cfc507
eJwtzF1rwyAUgOH/cq69MG7WztuVwsoK3ejK7sToSWpioxyT9Yv+90HJ7QvPewfrHJayTz0OoIFLt/ACbSVrXzvevMqlEE31gkJypbhQb3zJVaWAQab0FzzSTO/zyYzzqj0m87HpDl+Hzr5vqs/F925/o5/uJte0Pa5+h5b7S+5XwOBJzHjNCBpqtIQEDIpLz2D9KQw6T3UMzvR4ZYTW60QtI8yJTQVJ48mGyM6J+iamMzDASw6ExdgR9DDF+Hj8A/iRT7o= <xenia.aguilar.7e7@itb.cat>
2025-02-13 20:42:53 +0100

    Npc*** dubtes

--- Assets/Scripts/ExcelReader.cs
+++ Assets/Scripts/ExcelReader.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class ExcelReader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string csv = "Database";
+    public List<string> Answers, Questions;
+    public Button questionButton;
+    public TextMeshProUGUI answerText;
+    
     void Start()
     {
-        
+        TextAsset text = Resources.Load<TextAsset>(csv);
+        if(csv!= null)
+        {
+            ReadCSV(text.text);
+            answerText.text = "";
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void ReadCSV(string csv)
     {
-        
+        string[] rows = csv.Split("\n");
+        for (int i=0; i<rows.Length; i++)
+        {
+            string[] cells = rows[i].Split(",");
+            Questions.Add(cells[0]);
+            Button newQButton = Instantiate(questionButton, questionButton.transform.parent);
+            newQButton.GetComponentInChildren<TextMeshProUGUI>().text = cells[0];
+            var currentIndex = i;
+            newQButton.onClick.AddListener(() => AnswerTheQuestion(currentIndex));
+            /*newQButton.onClick.AddListener(delegate { 
+                AnswerTheQuestion(currentIndex);
+            } );*/
+            Answers.Add(cells[1]);
+        }
+        questionButton.gameObject.SetActive(false);
+    }
+    public void AnswerTheQuestion(int i)
+    {
+        answerText.text = Answers[i];
     }
 }
