cfcd0366cbc2501e7dec412e528a1619e4275bf3
eJwtjrtOAzEQAP9laxf3CDrHHRISpxRAEaC01vY6MefY1vqOEKL8OyK6dqQZzRXQWqp1nydKoMB40/hG2t4MrZQerez6bUNucBty/XbYGGrxQToQUDh/B0e8qte1pOc1dThmvR9fa9rhe+nHdnx5/n2cuxE/dse3T2/rU5e+0jKBgLui50uh/wVCJgYB1eY7QHcKSZXFxGD1RBfBhE5lPgimksVSiRWdMERxzjz5mM8ggH5KYKoaZ1BpifF2+wP+UFEZ <xenia.aguilar.7e7@itb.cat>
2025-06-06 13:51:20 +0200

    ffggghhhhg

--- Assets/Scripts/ExcelReader.cs
+++ Assets/Scripts/ExcelReader.cs
@@ -3,65 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+
 public class ExcelReader : MonoBehaviour
 {
     public string csv = "Database";
-    public List<string> Answers, Questions;
+    public List<string> Answers = new List<string>(), Questions = new List<string>();
     public Button questionButton;
     public TextMeshProUGUI answerText;
 
     public Button exitButton;
-
     public GameObject uiPanel;
 
-    
-    
+    // Referencias para controlar el jugador y cámaras
+    public PlayerMover playerMoverScript;
+    public GameObject joystickUI;
+    public Camera playerCamera;
+    public Camera npcCamera;
+
     void Start()
     {
         TextAsset text = Resources.Load<TextAsset>(csv);
-        if(csv!= null)
+        if (text != null)
         {
             ReadCSV(text.text);
             answerText.text = "";
         }
 
-        if(exitButton != null)
+        if (exitButton != null)
         {
             exitButton.onClick.AddListener(HideUIPanel);
         }
     }
-    private void ReadCSV(string csv)
+
+    private void ReadCSV(string csvText)
     {
-        string[] rows = csv.Split("\n");
-        for (int i=0; i<rows.Length; i++)
+        string[] rows = csvText.Split('\n');
+        for (int i = 0; i < rows.Length; i++)
         {
-            string[] cells = rows[i].Split(",");
+            string[] cells = rows[i].Split(',');
+            if(cells.Length < 2) continue;
             Questions.Add(cells[0]);
+            Answers.Add(cells[1]);
             Button newQButton = Instantiate(questionButton, questionButton.transform.parent);
             newQButton.GetComponentInChildren<TextMeshProUGUI>().text = cells[0];
-            var currentIndex = i;
+            int currentIndex = i;
             newQButton.onClick.AddListener(() => AnswerTheQuestion(currentIndex));
-            /*newQButton.onClick.AddListener(delegate { 
-                AnswerTheQuestion(currentIndex);
-            } );*/
-            Answers.Add(cells[1]);
-        }1
+        }
         questionButton.gameObject.SetActive(false);
     }
+
     public void AnswerTheQuestion(int i)
     {
         answerText.text = Answers[i];
     }
+
+    // Método para activar UI diálogo y bloquear jugador, activar cámara NPC
+    public void ShowUIPanel()
+    {
+        if (uiPanel != null)
+            uiPanel.SetActive(true);
+
+        if (playerMoverScript != null)
+            playerMoverScript.canMove = false;
+
+        if (joystickUI != null)
+            joystickUI.SetActive(false);
+
+        if (playerCamera != null)
+            playerCamera.gameObject.SetActive(false);
+
+        if (npcCamera != null)
+            npcCamera.gameObject.SetActive(true);
+    }
+
+    // Método para cerrar diálogo, activar movimiento, cámara jugador y joystick
     public void HideUIPanel()
     {
-        if(uiPanel != null)
-        {
+        if (uiPanel != null)
             uiPanel.SetActive(false);
-            
-        }
-         if (playerController != null)
-    {
-        playerController.SetActive(true); 
+
+        if (playerMoverScript != null)
+            playerMoverScript.canMove = true;
+
+        if (joystickUI != null)
+            joystickUI.SetActive(true);
+
+        if (npcCamera != null)
+            npcCamera.gameObject.SetActive(false);
+
+        if (playerCamera != null)
+            playerCamera.gameObject.SetActive(true);
     }
-}
 }
